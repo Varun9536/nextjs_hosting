8843e663256b557f67718a94ee526c86cd7c6dd6
Varun kumar <varunkumar953685@gmail.com>
2024-07-08 12:23:25 +0530

    commit

---
+++ src/app/components/Contacts/page.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+
+    <div>
+        my contact
+    </div>
+
+    <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nisi sed inventore quam nobis! Officia labore sint, quibusdam ipsam magnam delectus ex vero error nobis sed, repellendus dolores atque quasi illo, obcaecati voluptatum incidunt. Repellendus minima saepe temporibus rerum tempora.
+    </h3>
+
+    <h2>9536855214</h2>
+
+    <Link href={"/"} >go to home</Link>
+    
+    
+    </>
+  )
+}
---
+++ src/app/components/address/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function page() {
+  return (
+    <>
+    <h1>address</h1>
+
+    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et obcaecati nobis fugit sequi maxime, quidem veniam quisquam accusamus quod, repellat id asperiores provident nisi sint fuga, accusantium autem! Maxime officia facere sapiente dignissimos quae quaerat eligendi, excepturi aspernatur quisquam aliquam.</h2>
+    
+
+    <h1>noida sector 59</h1>
+
+    <Link href={"/"} >go to home</Link>
+
+
+    </>
+  )
+}
